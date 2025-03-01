9cba0ad3d82ee4d2f0f5971b5b9ac0a62aa8c3bf
Oleg <oleg.saltykov@gmail.com>
2025-03-01 17:56:02 -0500

    test fix

--- tests/user_model_test.rb
+++ tests/user_model_test.rb
@@ -2,12 +2,15 @@ require_relative 'test_helper'
 
 describe 'User model' do
   describe ".create" do
+    after do
+      User.destroy_all
+    end
+
     describe 'when data is valid' do
       it 'persists to db' do
         user = User.create(email: "ok-test@test.com", password: "123")
 
          _(user.persisted?).must_equal true
-         User.destroy_all
       end
     end
 
@@ -17,7 +20,6 @@ describe 'User model' do
         User.create(wrong: "test@test") rescue nil
 
         _(User.all.count).must_equal initial_count
-        User.destroy_all
       end
     end
   end
