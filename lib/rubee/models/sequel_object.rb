31866b34e358dc9d27066ec242c4d976016cb79c
Oleg <oleg.saltykov@gmail.com>
2025-04-08 18:12:08 -0400

    reported bug fix
    https://github.com/nucleom42/rubee/issues/14

--- lib/rubee/models/sequel_object.rb
+++ lib/rubee/models/sequel_object.rb
@@ -21,11 +21,19 @@ module Rubee
     def save
       args = to_h.dup&.transform_keys(&:to_sym)
       if args[:id]
-        udpate(args) rescue return false
+        begin
+          udpate(args)
+        rescue => _
+          return false
+        end
 
         true
       else
-        created_object = self.class.create(args) rescue return false
+        begin
+          created_object = self.class.create(args)
+        rescue => _
+          return false
+        end
         self.id = created_object.id
 
         true
