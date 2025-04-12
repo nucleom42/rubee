4eb755a6009db439e08e0b1389695f679b5a2700
Oleg <oleg.saltykov@gmail.com>
2025-04-12 12:36:14 -0400

    build new version

--- lib/rubee.rb
+++ lib/rubee.rb
@@ -12,7 +12,7 @@ module Rubee
   APP_ROOT = File.expand_path(Dir.pwd) unless defined?(APP_ROOT)
   IMAGE_DIR = File.join(APP_ROOT, 'images') unless defined?(IMAGE_DIR)
   PROJECT_NAME = File.basename(APP_ROOT) unless defined?(PROJECT_NAME)
-  VERSION = '1.3.1'
+  VERSION = '1.3.3'
 
   class Application
     include Singleton
