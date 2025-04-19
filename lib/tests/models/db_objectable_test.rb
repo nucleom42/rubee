9680b1cf92250c9338071ca4fd8b372560e158e5
Phil Hendricks <pfhendri1@gmail.com>
2025-04-19 14:02:17 -0500

    removed singular plural tests

--- lib/tests/models/db_objectable_test.rb
+++ lib/tests/models/db_objectable_test.rb
@@ -11,19 +11,6 @@ describe 'Database Objectable' do
       _(MergBerg.pluralize_class_name).must_equal('mergbergs')
     end
 
-    it 'pluralizes words' do
-      _(MergBerg.pluralize('pony')).must_equal('ponies')
-      _(MergBerg.pluralize('hand')).must_equal('hands')
-      _(MergBerg.pluralize('fox')).must_equal('foxes')
-    end
-
-    it 'singularizes words' do
-      _(MergBerg.singularize('ponies')).must_equal('pony')
-      _(MergBerg.singularize('hands')).must_equal('hand')
-      _(MergBerg.singularize('foxes')).must_equal('fox')
-      _(MergBerg.singularize('moon')).must_equal('moon')
-    end
-
     it 'retrieves accessor names' do
       accessors = MergBerg.accessor_names
       _(accessors).must_include(:id)
