1f5a4af797855d49fddfde02f578f39f7cc59aab
Oleg <oleg.saltykov@gmail.com>
2025-12-05 18:20:08 -0500

    2.2.2
    decode jwt bugfix

--- lib/inits/charged_hash.rb
+++ lib/inits/charged_hash.rb
@@ -12,5 +12,27 @@ module ChargedHash
     def wrap(value)
       value.is_a?(Hash) ? value.extend(ChargedHash) : value
     end
+
+    def keys_to_string!
+      keys_to(:string, self)
+    end
+
+    def keys_to_sym!
+      keys_to(:symbol, self)
+    end
+
+    def keys_to(type, obj)
+      case obj
+      when Hash
+        obj.each_with_object({}) do |(k, v), result|
+          key = k.to_s
+          result[key] = keys_to(type, v)
+        end
+      when Array
+        obj.map { |v| keys_to(type, v) }
+      else
+        obj
+      end
+    end
   end
 end
