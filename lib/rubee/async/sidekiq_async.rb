5aac5fbc08a730088f8c22044d1c119bac567604
Oleg <oleg.saltykov@gmail.com>
2026-03-14 11:26:57 -0400

    update sidekiq async

--- lib/rubee/async/sidekiq_async.rb
+++ lib/rubee/async/sidekiq_async.rb
@@ -1,15 +1,48 @@
 module Rubee
   class SidekiqAsync
     def perform_async(**args)
-      options = if args[:options].is_a?(Hash)
-        [JSON.generate(args[:options])]
-      elsif args[:options].is_a?(Array)
-        args[:options]
-      else
-        [args[:options]]
+      options = serialize_options(args[:options])
+      args[:_class].perform_async(*options)
+    end
+
+    def perform_at(interval, **args)
+      options = serialize_options(args[:options])
+      args[:_class].perform_at(interval, *options)
+    end
+
+    def perform_in(interval, **args)
+      options = serialize_options(args[:options])
+      args[:_class].perform_in(interval, *options)
+    end
+
+    def perform_later(interval, **args)
+      perform_in(interval, **args)
+    end
+
+    def perform_bulk(jobs_args)
+      jobs_args.map! do |args|
+        options = serialize_options(args[:options])
+        { args: options }
       end
 
-      args[:_class].perform_async(*options)
+      args[:_class].perform_bulk(jobs_args)
+    end
+
+    def set(options, **args)
+      serialized_options = serialize_options(args[:options])
+      args[:_class].set(options).perform_async(*serialized_options)
+    end
+
+    private
+
+    def serialize_options(options)
+      if options.is_a?(Hash)
+        [JSON.generate(options)]
+      elsif options.is_a?(Array)
+        options
+      else
+        [options]
+      end
     end
   end
 end
